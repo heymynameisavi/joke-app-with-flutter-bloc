f85c59e1a61a9a7926d582fe4d0fa7715ec243ee
Abhilash Jha <34411633+heymynameisavi@users.noreply.github.com>
2022-02-18 19:14:27 +0530

    Update constants.dart
    changed constants file

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -7,5 +7,5 @@ class Constants {
   // Constants Staing
 
   static const String apiHost = 'jokeapi-v2.p.rapidapi.com'; //https://rapidapi.com/Sv443/api/jokeapi-v2
-  static const String apiKey = '2567c92d0amshc1594fcc3ad81efp1ce097jsn2853730d8b8e';
+  static const String apiKey = '<Your API KEY>';
 }
